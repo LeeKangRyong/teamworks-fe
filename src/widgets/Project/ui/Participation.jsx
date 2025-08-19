c3b186175340e625cb2c5338507d1de1d12358f0
LeeKangRyong <gaiogo2@naver.com>
2025-08-19 19:10:20 +0900

    상세 프로젝트 UI 배경 고정

--- src/widgets/Project/ui/Participation.jsx
+++ src/widgets/Project/ui/Participation.jsx
@@ -2,13 +2,7 @@ import { Box } from "@/entities/project/dashboard";
 
 export function Participation() {
     return (
-        <main className="bg-white w-full max-w-none py-4">
-            <article className="flex flex-row gap-6 w-full min-w-full px-12">
-                <Box num="25" desc="총 참여 팀" />
-                <Box num="3" desc="주의 요망 팀" />
-                <Box num="85%" desc="과제 제출률" />
-                <Box num="D-8" desc="다음 과제 제출일" />
-            </article>
+        <main className="bg-white w-250 py-4 mb-10">
         </main>
     );
 }
