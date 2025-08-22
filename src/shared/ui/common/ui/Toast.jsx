96e2757de22653470a788f25c2a62a7fc162ddd1
LeeKangRyong <gaiogo2@naver.com>
2025-08-22 17:41:31 +0900

    Toast 위치 top-165 -> bottom-5로 변경;;

--- src/shared/ui/common/ui/Toast.jsx
+++ src/shared/ui/common/ui/Toast.jsx
@@ -3,7 +3,7 @@ import success from "@/assets/icons/success-white.png";
 
 export function Toast({ message }) {
     return (
-        <div className="fixed left-1/2 -translate-x-1/2 top-165 animate-pulse animate-slideUpAndFade">
+        <div className="fixed left-1/2 -translate-x-1/2 bottom-5 animate-pulse animate-slideUpAndFade">
             <div className="fixed inset-0 bg-white rounded-lg"></div> {/* Toast 뒤 안보이게 하기 (bg-toast에 opacity-80 적용됨 issuse) */}
             <div className="flex flex-row w-fit gap-1 px-3 py-4 bg-toast rounded-lg items-center justify-center relative z-10">
                 <Image src={success} alt="success" className="w-4 y-4" />
