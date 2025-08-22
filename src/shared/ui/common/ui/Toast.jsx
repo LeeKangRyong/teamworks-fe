07c4d469d6c05fa910ae3def836d2a2fbcdea90e
LeeKangRyong <gaiogo2@naver.com>
2025-08-22 16:38:30 +0900

    Toast 위치(우측 상단 -> 중앙 하단) 및 배경 색깔 변경

--- src/shared/ui/common/ui/Toast.jsx
+++ src/shared/ui/common/ui/Toast.jsx
@@ -3,8 +3,9 @@ import success from "@/assets/icons/success-white.png";
 
 export function Toast({ message }) {
     return (
-        <div className="absolute right-4 top-20 animate-pulse animate-slideUpAndFade">
-            <div className="flex flex-row w-fit gap-1 px-3 py-4 bg-secondary-40 rounded-lg items-center justify-center">
+        <div className="fixed left-1/2 -translate-x-1/2 top-165 animate-pulse animate-slideUpAndFade">
+            <div className="fixed inset-0 bg-white rounded-lg"></div> {/* Toast 뒤 안보이게 하기 (bg-toast에 opacity-80 적용됨 issuse) */}
+            <div className="flex flex-row w-fit gap-1 px-3 py-4 bg-toast rounded-lg items-center justify-center relative z-10">
                 <Image src={success} alt="success" className="w-4 y-4" />
                 <p className="text-gray-0 text-body-s text-center">{message}</p>
             </div>
