926e55ca6cb5b7b324fd46212913d5264dc9aa9c
LeeKangRyong <gaiogo2@naver.com>
2025-08-24 20:56:14 +0900

    Revert "globals.css에서 min-width: 100vw; 설정 issue로 인해 Login.jsx 내용이 왼쪽으로 치우쳐짐 -> 중앙 component를 justify-center로 설정"
    This reverts commit 1726efa29d4628a58d08864495ba74b3a55f17af.

--- src/pages/auth/ui/Login.jsx
+++ src/pages/auth/ui/Login.jsx
@@ -9,17 +9,15 @@ import google from "@/assets/icons/google.png";
 
 export function Login() {
     return (
-        <div className="min-h-screen w-full flex flex-col">
-            <header className="absolute w-full h-18 bg-gray-0 top-0 flex items-center px-4 border-b-1 border-gray-10 z-50">
-                <div className="w-full max-w-screen-xl flex justify-between items-center">
-                    <div className="flex flex-row gap-2 items-center">
-                        <Image src={logo} alt="logo" className="rounded-sm max-h-6 w-auto" />
-                        <p className="text-body-l font-black">TeamWorks</p>
-                    </div>
+        <div className="min-h-screen flex flex-col">
+            <header className="absolute w-full h-18 bg-gray-0 top-0 flex justify-between items-center px-4 border-b-1 border-gray-10 z-50">
+                <div className="flex flex-row gap-2 items-center">
+                    <Image src={logo} alt="logo" className="rounded-sm max-h-6 w-auto" />
+                    <p className="text-body-l font-black">TeamWorks</p>
                 </div>
             </header>
             
-            <div className="flex-1 w-full flex flex-col justify-center items-center gap-8 pt-18">
+            <div className="flex-1 flex flex-col justify-center items-center gap-8 pt-18">
                 <Image src={thumbnail} alt="thumbnail" className="h-30 w-auto" />
                 <p className="text-body-l text-gray-70 text-center">공정한 협업, 효율적인 운영</p>
                 <Image src={google} alt="google" className="h-10 w-auto"/>
