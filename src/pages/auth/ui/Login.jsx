c49c254572c04a249345bc571453e3b701305f2c
LeeKangRyong <gaiogo2@naver.com>
2025-08-26 15:29:06 +0900

    로고 및 글씨 클릭 시 /projects로 이동 + Login 페이지에서 바로 이동하는 개발용 버튼 만들어놓음

--- src/pages/auth/ui/Login.jsx
+++ src/pages/auth/ui/Login.jsx
@@ -1,3 +1,8 @@
+// 개발용
+"use client";
+import { useRouter } from "next/navigation";
+// 
+
 import Image from "next/image";
 import logo from "@/assets/icons/logo.png";
 import user from "@/assets/icons/user.png";
@@ -7,7 +12,17 @@ import arrow_down from "@/assets/icons/arrow-down.png";
 import thumbnail from "@/assets/icons/thumbnail.png"
 import google from "@/assets/icons/google.png";
 
+
+
 export function Login() {
+    
+    // 개발용
+    const router = useRouter();
+    const handleLogo = () => {
+        router.push("/projects");
+    };
+    //
+
     return (
         <div className="min-h-screen flex flex-col">
             <header className="absolute w-full h-18 bg-gray-0 top-0 flex justify-between items-center px-4 border-b-1 border-gray-10 z-50">
@@ -22,6 +37,12 @@ export function Login() {
                 <p className="text-body-l text-gray-70 text-center">공정한 협업, 효율적인 운영</p>
                 <Image src={google} alt="google" className="h-10 w-auto"/>
             </div>
+
+            {/* 개발용 */}
+            <button className="absolute top-40 left-1/2 -translate-x-1/2 bg-warning-100 w-35 px-3 py-2 items-center rounded" onClick={handleLogo}>
+                <span className="text-body-m text-gray-0 text-center">/projects로 가기</span>
+            </button>
+            {/* */}
         </div>
     );
 }
--- src/widgets/Layout/ui/LayoutHeader.jsx
+++ src/widgets/Layout/ui/LayoutHeader.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import logo from "@/assets/icons/logo.png";
 import { HeaderList } from "@/features/layout";
@@ -7,20 +8,25 @@ import { useToast } from "@/shared/hooks";
 import { Toast } from "@/shared/ui/common";
 
 export function LayoutHeader() {
+    const router = useRouter();
     const { isVisible, message, showToast } = useToast();
 
     const handleToastTest = () => {
         showToast("토스트 메세지입니다");
     };
 
+    const handleLogo = () => {
+        router.push("/projects");
+    };
+
     return (
-        <header className="absolute w-full h-18 bg-gray-0 top-0 flex justify-between items-center px-4 border-b-1 border-gray-10 z-50">
-            <div className="flex flex-row gap-2 items-center">
+        <header className="absolute w-full h-18 bg-gray-0 top-0 flex justify-between items-center border-b-1 border-gray-10 z-50">
+            <div className="flex flex-row gap-2 items-center cursor-pointer px-4" onClick={handleLogo}>
                 <Image src={logo} alt="logo" className="rounded-sm max-h-6 w-auto" />
                 <p className="text-body-l font-black">TeamWorks</p>
             </div>
             <HeaderList />
-            {/* Toast test button */}
+
             <button
                 className="absolute top-5 right-40 bg-warning-100 w-fit text-center text-gray-0 text-body-s p-2 rounded-full hover:scale-105 duration-200"
                 onClick={handleToastTest}   
@@ -28,6 +34,7 @@ export function LayoutHeader() {
                 토스트 테스트
             </button>            
             {isVisible && <Toast message={message} />}
+
         </header>
     );
 }
