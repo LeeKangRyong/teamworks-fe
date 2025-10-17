0699027348a374d0689e1bfeb797331104156e9d
LeeKangRyong <gaiogo2@naver.com>
2025-10-17 19:20:38 +0900

    index 오타 수정

--- src/entities/project/dashboard/index.ts
+++ src/entities/project/dashboard/index.ts
@@ -9,5 +9,3 @@ export { Box } from "./ui/Box"
 export { NextSubmit } from "./ui/NextSubmit"
 export { TeamSummary } from "./ui/TeamSummary"
 export { ToDoSummary } from "./ui/ToDoSummary"
-
-export { createChartData, chartOptions, createCustomLabelsPlugin } from "./config/chartConfig"
