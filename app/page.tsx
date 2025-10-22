ee991c870452456563193d475d29fcf5e82da5ca
LeeKangRyong <gaiogo2@naver.com>
2025-10-23 03:21:41 +0900

    CSR 이슈로 인하여, 로그인 상태로 / 진입시 /projects로 이동하도록 조치

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import { Main } from "@/pages/auth";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { tokenStorage } from "@/shared/lib/tokenStorage";
 
 export default function HomePage() {
-  return (
-    <Main />
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    if (tokenStorage.hasValidToken()) {
+      router.replace('/projects');
+    }
+  }, [router]);
+
+  return <Main />;
 }
