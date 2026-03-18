f02dcef4c975e39fba62251d2bdf26a1378e6a31
LeeKangRyong <gaiogo2@naver.com>
2026-03-18 16:12:25 +0900

    feat: Set endpoint and auth header #12

--- src/shared/api/endpoints.js
+++ src/shared/api/endpoints.js
@@ -1,22 +1,43 @@
-// 일단 관리자만 연결
 export const ENDPOINTS = {
     AUTH: {
-        LOGIN: '/api/auth/login',
+        LOGIN:          '/api/auth/login',
         REGISTER_ADMIN: '/api/auth/register/admin',
-        REGISTER_USER: '/api/auth/register/user',
-        REFRESH: '/api/auth/refresh',
-        LOGOUT: '/api/auth/logout'
+        REGISTER_USER:  '/api/auth/register/user',
+        REFRESH:        '/api/auth/refresh',
+        LOGOUT:         '/api/auth/logout',
     },
 
     PROJECTS: {
-        MY: '/api/projects/my',
-        DETAIL: (projectId) => `/api/projects/${projectId}`,
-        CREATE: '/api/projects',
-        UPDATE: (projectId) => `/api/projects/${projectId}`,
-        DELETE: (projectId) => `/api/projects/${projectId}`,
-        PARTICIPANTS: (projectId) => `/api/projects/${projectId}/participants`
+        MY:             '/api/projects/my',
+        PARTICIPATING:  '/api/projects/participating',
+        CREATE:         '/api/projects',
+        DETAIL:         (id) => `/api/projects/${id}`,
+        UPDATE:         (id) => `/api/projects/${id}`,
+        DELETE:         (id) => `/api/projects/${id}`,
+        PARTICIPANTS:   (id) => `/api/projects/${id}/participants`,
     },
+
     PROJECT: {
-        
-    }
+        DASHBOARD:      (pId) => `/api/projects/${pId}/dashboard`,
+
+        ASSIGNMENTS:    (pId) => `/api/projects/${pId}/assignments`,
+        ASSIGNMENT:     (pId, aId) => `/api/projects/${pId}/assignments/${aId}`,
+        SUBMITS:        (pId, aId) => `/api/projects/${pId}/assignments/${aId}/submits`,
+        SUBMIT:         (pId, aId, sId) => `/api/projects/${pId}/assignments/${aId}/submits/${sId}`,
+        MY_SUBMIT:      (pId, aId) => `/api/projects/${pId}/assignments/${aId}/my-submit`,
+        SUBMIT_MEMO:    (pId, aId, sId) => `/api/projects/${pId}/assignments/${aId}/submits/${sId}/memo`,
+        CHART:          (pId) => `/api/projects/${pId}/chart`,
+
+        TEAMS:          (pId) => `/api/projects/${pId}/teams`,
+        TEAM:           (pId, tId) => `/api/projects/${pId}/teams/${tId}`,
+
+        STUDENTS:       (pId) => `/api/projects/${pId}/students`,
+        STUDENT:        (pId, sId) => `/api/projects/${pId}/students/${sId}`,
+
+        CHATS:          (pId) => `/api/projects/${pId}/chats`,
+        CHAT:           (pId, cId) => `/api/projects/${pId}/chats/${cId}`,
+        MESSAGES:       (pId, cId) => `/api/projects/${pId}/chats/${cId}/messages`,
+
+        MANAGERS:       (pId) => `/api/projects/${pId}/managers`,
+    },
 };
